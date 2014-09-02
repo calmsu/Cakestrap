197982dec70efa6ad79cb85be450503acecff561
Brian Adams <wreality@gmail.com>
2014-09-02 15:18:38 -0400

    Component now passes options to helpers if provided.

--- Controller/Component/CakestrapComponent.php
+++ Controller/Component/CakestrapComponent.php
@@ -40,29 +40,54 @@ class CakestrapComponent extends Component {
 	public function initialize(Controller $controller) {
 		parent::initialize($controller);
 		//If helpers is true, replace the Html and Form helpers with ours.
+		foreach (array_keys($this->helpers) as $key) {
+			if (is_int($key)) {
+				$this->helpers[$this->helpers[$key]] = true;
+				unset($this->helpers[$key]);
+			}
+		}
 		if ($this->helpers) {
-			if (($key = array_search('Form', $controller->helpers)) !== false) {
-				unset($controller->helpers[$key]);
-				$controller->helpers['Form'] = true;
+			if ((is_array($this->helpers) && array_key_exists('Form', $this->helpers)) || ($this->helpers === true)) {
+				if (($key = array_search('Form', $controller->helpers)) !== false) {
+					unset($controller->helpers[$key]);
+					$controller->helpers['Form'] = true;
+				}
+				if (array_key_exists('Form', $controller->helpers)) {
+					if ((!empty($this->helpers['Form'])) && is_array($this->helper['Form'])) {
+						$controller->helpers['Form'] = array_merge(['className' => 'Cakestrap.CakestrapForm'], $this->helpers['Form']);
+					} else {
+						$controller->helpers['Form'] = array('className' => 'Cakestrap.CakestrapForm');
+					}
+				}
 			}
-			if (array_key_exists('Form', $controller->helpers)) {
-				$controller->helpers['Form'] = array('className' => 'Cakestrap.CakestrapForm');
-			}
-			if (($key = array_search('Html', $controller->helpers)) !== false) {
-				unset($controller->helpers[$key]);
-				$controller->helpers['Html'] = true;
-			}
-			if (array_key_exists('Html', $controller->helpers)) {
-				$controller->helpers['Html'] = array('className' => 'Cakestrap.CakestrapHtml');
-			}
-			if (($key = array_search('Paginator', $controller->helpers)) !== false) {
-				unset($controller->helpers[$key]);
-				$controller->helpers['Paginator'] = true;
-			}
-			if (array_key_exists('Paginator', $controller->helpers)) {
-				$controller->helpers['Paginator'] = array('className' => 'Cakestrap.CakestrapPaginator');
-			}
+			if ((is_array($this->helpers) && array_key_exists('Html', $this->helpers)) || ($this->helpers === true)) {
+				if (($key = array_search('Html', $controller->helpers)) !== false) {
+					unset($controller->helpers[$key]);
+					$controller->helpers['Html'] = true;
+				}
+				if (array_key_exists('Html', $controller->helpers)) {
+					if ((!empty($this->helpers['Html'])) && is_array($this->helper['Html'])) {
+						$controller->helpers['Html'] = array_merge(['className' => 'Cakestrap.CakestrapHtml'], $this->helpers['Html']);
+					} else {
+						$controller->helpers['Html'] = array('className' => 'Cakestrap.CakestrapHtml');
+					}
 
+				}
+			}
+			if ((is_array($this->helpers) && array_key_exists('Paginator', $this->helpers)) || ($this->helpers === true)) {
+				if (($key = array_search('Paginator', $controller->helpers)) !== false) {
+					unset($controller->helpers[$key]);
+					$controller->helpers['Paginator'] = true;
+				}
+				if (array_key_exists('Paginator', $controller->helpers)) {
+					if ((!empty($this->helpers['Paginator'])) && is_array($this->helper['Paginator'])) {
+						$controller->helpers['Paginator'] = array_merge(['className' => 'Cakestrap.CakestrapPaginator'], $this->helpers['Paginator']);
+					} else {
+						$controller->helpers['Paginator'] = array('className' => 'Cakestrap.CakestrapPaginator');
+					}
+
+				}
+			}
 		}
 
 		/** Setup our Defines **/
